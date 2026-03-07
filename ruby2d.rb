310c43c1c1f1c05abee327fa05500d313b068d09
niwdE293 <edwin.adel@elev.ga.ntig.se>
2026-03-07 13:23:12 +0100

    Update ruby2d.rb

--- ruby2d.rb
+++ ruby2d.rb
@@ -1,41 +1,77 @@
 require 'ruby2d'
 
-@player = Square.new(x: 10, y: 20, size: 25, color: 'blue')
-@player_speed = 3
+class Player
+  SIZE = 25
+  START_POS_X = 10
+  START_POS_Y = 20
+  COLOR = 'blue'
+  SPEED = 3
 
-@x_speed = 0
-@y_speed = 0
+  attr_accessor :x_speed, :y_speed, :hitbox , :x, :y
+  def initialize()
+    @x_speed = 0
+    @y_speed = 0
+    @x = START_POS_X
+    @y = START_POS_Y
+    @hitbox = Square.new(x: START_POS_X, y: START_POS_Y, size: SIZE, color: COLOR)
+  end
+
+  def update()
+    @hitbox.x += @x_speed
+    @hitbox.y += @y_speed
+  end
+
+  def collided()
+    if (r1x + r1w >= r2x &&    // r1 right edge past r2 left
+      r1x <= r2x + r2w &&    // r1 left edge past r2 right
+      r1y + r1h >= r2y &&    // r1 top edge past r2 bottom
+      r1y <= r2y + r2h) {    // r1 bottom edge past r2 top
+        return true;
+  }
+  return false;
+}
+  end
+end
+
+
+
+class Object
+  attr_accessor :x , :y, :width, :height, :color
+  def initialize(x, y, width, height, color)
+    @object = Rectangle.new(x: x, y: y, width: width, height: height, color: color)
+  end
+end
+
 
 set fps_cap: 60
 
-@rectangle = Rectangle.new(x: (get :width) / 2, y: (get :height) / 2, width: 200, height: 20, color: 'white')
+@player = Player.new()
+
+@object = Object.new(400, 300, 100, 10, 'blue')
+
 
 on :key_held do |event|
   if event.key == 'w'
-    @y_speed = - @player_speed
+    @player.y_speed = - Player::SPEED
   elsif event.key == 'a'
-    @x_speed = - @player_speed
+    @player.x_speed = - Player::SPEED
   elsif event.key == 's'
-    @y_speed = @player_speed
+    @player.y_speed = Player::SPEED
   elsif event.key == 'd'
-    @x_speed = @player_speed
+    @player.x_speed = Player::SPEED
   end
 end
 
 on :key_up do |event|
   if event.key == 'w'|| event.key == 's'
-    @y_speed = 0
+    @player.y_speed = 0
   elsif event.key == 'a' || event.key == 'd'
-    @x_speed = 0
+    @player.x_speed = 0
   end
 end
 
 update do
-  @player.x += @x_speed
-  @player.y += @y_speed
+  @player.update
 end
-
-if @player
-
 
 show
